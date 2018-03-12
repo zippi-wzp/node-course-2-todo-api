c7a91c0fdc3246f20bef7bd7738e4ca7ed52d338
Yeryshev Artem <truebatmandc@gmail.com>
2018-03-12 19:03:11 +0200

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    console.log('Unable to connect to mongodb server');
+    return;
+  }
+  console.log('Connected to mongodb server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5aa6aa2435bd9d7176a10e46')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({name: 'Artem'}).count().then((count) => {
+    console.log(`Todos count: ${count}`);
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+  // client.close();
+});
